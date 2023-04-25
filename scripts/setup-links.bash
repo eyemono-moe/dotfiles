23131bfdfe074bc9af748e33f1184de564710f0d
detteiu8383 <ysdr83@gmail.com>
2023-04-25 10:17:47 +0000

    add code path

--- scripts/setup-links.bash
+++ scripts/setup-links.bash
@@ -21,5 +21,5 @@ ln -sfnv "$XDG_CONFIG_HOME/gitmoji" "$HOME/.gitmoji"
 ln -sfnv "$XDG_CONFIG_HOME/vim" "$HOME/.vim"
 
 # bin PATH
-sudo ln -sf "$REPO_DIR/config/bin" "/usr/local"
+sudo ln -sf "$REPO_DIR/config/bin" "/usr/local/bin"
 
