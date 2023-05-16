26f8c6db7bed81be3031b3db963150a23ac45b44
detteiu8383 <>
2023-05-17 05:46:58 +0900

    docker: add -y option at install

--- scripts/installer/docker.bash
+++ scripts/installer/docker.bash
@@ -18,5 +18,5 @@ else
 
   sudo apt update
 
-  sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
