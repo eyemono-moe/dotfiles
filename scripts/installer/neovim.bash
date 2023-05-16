c156c36d0eab02394eb90a7a67a9a26c0fda328e
detteiu8383 <>
2023-05-16 13:10:33 +0900

    fix nvim installation

--- scripts/installer/neovim.bash
+++ scripts/installer/neovim.bash
@@ -3,7 +3,13 @@
 if (type "nvim" > /dev/null 2>&1); then
   echo "nvim is already installed."
 else
+  # see https://github.com/neovim/neovim/wiki/Installing-Neovim#appimage-universal-linux-package
   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
   chmod u+x nvim.appimage
-  ./nvim.appimage
+  ./nvim.appimage --appimage-extract
+  ./squashfs-root/AppRun --version
+
+  # Optional: exposing nvim globally.
+  sudo mv squashfs-root /
+  sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
 fi
