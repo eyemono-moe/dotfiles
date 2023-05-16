63eb43afa051d8da5c2f34039bc2a9a032c70ba8
detteiu8383 <>
2023-05-17 06:15:29 +0900

    node: add sudo

--- scripts/installer/node.bash
+++ scripts/installer/node.bash
@@ -5,6 +5,6 @@ if (type "node" > /dev/null 2>&1); then
 else
   # https://github.com/tj/n#installation
   sudo curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | sudo bash -s lts
-  npm install -g n
+  sudo npm install -g n
   sudo n stable
 fi
