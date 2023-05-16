15cb2bb9140d0b9fbb75ed04016a10bb9d0c0c9c
detteiu8383 <>
2023-05-16 22:52:58 +0900

    apt: add -y option

--- scripts/installer/node.bash
+++ scripts/installer/node.bash
@@ -4,9 +4,9 @@ if (type "node" > /dev/null 2>&1); then
   echo "node is already installed."
 else
   sudo apt update
-  sudo apt install nodejs npm
+  sudo apt -y install nodejs npm
   sudo npm -g install n
   sudo n stable
-  sudo apt purge nodejs npm
-  sudo apt autoremove
+  sudo apt -y purge nodejs npm
+  sudo apt -y autoremove
 fi
