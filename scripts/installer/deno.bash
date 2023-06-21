5835df571d660a84d4767f6e125262f81e561630
detteiu8383 <ysdr83@gmail.com>
2023-06-22 01:07:55 +0900

    :bug: fix deno install path

--- scripts/installer/deno.bash
+++ scripts/installer/deno.bash
@@ -4,6 +4,7 @@ if (type "deno" >/dev/null 2>&1); then
   echo "deno is already installed."
 else
   echo "deno is not installed. Install deno..."
+  export DENO_INSTALL="${DENO_INSTALL:-$XDG_DATA_HOME/deno}"
   curl -fsSL https://deno.land/x/install/install.sh | /bin/sh
 
   echo "Install Deno completions..."
