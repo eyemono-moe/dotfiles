4b9880cb94faaf4d9d9a61dd115a253af78e352f
detteiu8383 <ysdr83@gmail.com>
2023-11-08 21:03:26 +0900

    :adhesive_bandage: add dependencies of pyenv

--- scripts/installer/pyenv.bash
+++ scripts/installer/pyenv.bash
@@ -3,5 +3,22 @@
 if (type "pyenv" > /dev/null 2>&1); then
   echo "pyenv is already installed."
 else
+  apt update
+  sudo apt install -y \
+    build-essential \
+    curl \
+    libbz2-dev \
+    libffi-dev \
+    liblzma-dev \
+    libncursesw5-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    libxml2-dev \
+    libxmlsec1-dev \
+    tk-dev \
+    xz-utils \
+    zlib1g-dev
+
   curl https://pyenv.run | bash
 fi
