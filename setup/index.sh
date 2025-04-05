349e6cac228b43cdd4bc1325769b2d1ada77af67
eyemono-moe <ysdr83@gmail.com>
2025-04-06 07:02:36 +0900

    refactor: move environment variable exports to setup scripts for chezmoi

---
+++ setup/index.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euxo pipefail
+
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
+export XDG_CACHE_HOME="$HOME/.cache"
+
+# Install chezmoi
+if ! command -v chezmoi &>/dev/null; then
+  echo "[+] Installing chezmoi..."
+  sh -c "$(curl -fsLS get.chezmoi.io)"
+fi
+
+# Run chezmoi init
+~/bin/chezmoi init eyemono-moe --apply
