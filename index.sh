6ac429babd96015506a72701048f4b368756c690
eyemono-moe <ysdr83@gmail.com>
2025-04-07 04:49:08 +0900

    fix: add informative messages and environment variable exports in setup scripts

--- index.sh
+++ index.sh
@@ -7,6 +7,8 @@ case "${DEBUG:-}" in
     ;;
 esac
 
+echo -e "\033[1;34m[INFO]\033[0m    Welcome to the dot.eyemono.moe setup script!"
+
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_STATE_HOME="$HOME/.local/state"
@@ -16,6 +18,7 @@ export XDG_CACHE_HOME="$HOME/.cache"
 if ! command -v chezmoi &>/dev/null || [ ! -x "$HOME/.local/bin/chezmoi" ]; then
   echo -e "\033[1;34m[INFO]\033[0m    Installing chezmoi..."
   sh -c "$(curl -fsLS get.chezmoi.io)" -- -b $HOME/.local/bin
+  echo -e "\033[1;32m[SUCCESS]\033[0m chezmoi installed"
 fi
 
 # Run chezmoi init
