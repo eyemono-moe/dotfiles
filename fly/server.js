1f4ec416eea0db289c5fb3a887597a1d0bf1f697
eyemono-moe <ysdr83@gmail.com>
2025-04-06 12:13:02 +0900

    feat: add Dockerfile and .dockerignore; update fly.toml and server.js for improved configuration and redirect handling

--- fly/server.js
+++ fly/server.js
@@ -2,12 +2,12 @@ const http = require('node:http')
 
 const redirectUrl = 'https://raw.githubusercontent.com/eyemono-moe/dotfiles/refs/heads/main/index.sh';
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((_, res) => {
   res.writeHead(301, { Location: redirectUrl });
   res.end();
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`Redirect server running on port ${port}…`);
 });
