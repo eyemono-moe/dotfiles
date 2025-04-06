834647b291a01e6ed8c474f82869610eb3b52a4e
eyemono-moe <ysdr83@gmail.com>
2025-04-06 10:20:46 +0900

    feat: update brew installation script and add aqua installation script

--- run_30-brew-install.sh
+++ run_30-brew-install.sh
@@ -3,7 +3,12 @@ set -euxo pipefail
 
 export XDG_CONFIG_HOME="${HOME}/.config"
 
-if command -v brew &>/dev/null; then
-  echo "[+] Installing tools with brew bundle..."
-  brew bundle --file="$XDG_CONFIG_HOME/homebrew/Brewfile" || true
-fi
+{{ if eq .chezmoi.os "linux" }}
+BREW_PREFIX="/home/linuxbrew/.linuxbrew"
+{{ else if eq .chezmoi.os "darwin" }}
+BREW_PREFIX="/opt/homebrew"
+{{ end }}
+eval "$(${BREW_PREFIX}/bin/brew shellenv)"
+
+echo "[+] Installing tools with brew bundle..."
+brew bundle --file="$XDG_CONFIG_HOME/homebrew/Brewfile" || true
