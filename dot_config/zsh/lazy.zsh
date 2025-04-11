1a4c30b01e74f2f678d5a4b49d8c5c9376f372e3
eyemono-moe <yoshida@pocketsign.co.jp>
2025-04-11 16:56:23 +0900

    feat: add npm configuration settings and template for dot_npmrc

--- dot_config/zsh/lazy.zsh
+++ dot_config/zsh/lazy.zsh
@@ -128,6 +128,12 @@ fi
 ### Node.js ###
 export NODE_REPL_HISTORY="$XDG_STATE_HOME/node_history"
 
+### npm ###
+export NPM_CONFIG_DIR="$XDG_CONFIG_HOME/npm"
+export NPM_DATA_DIR="$XDG_DATA_HOME/npm"
+export NPM_CACHE_DIR="$XDG_CACHE_HOME/npm"
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_DIR/npmrc"
+
 ### PostgreSQL ###
 export PSQL_HISTORY="$XDG_STATE_HOME/psql_history"
 
