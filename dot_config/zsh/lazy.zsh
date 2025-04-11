4fa882a79a3efd1589c023d4d3343fc9f1cb487e
eyemono-moe <yoshida@pocketsign.co.jp>
2025-04-11 17:00:01 +0900

    fix: update npm user configuration file name in zsh settings

--- dot_config/zsh/lazy.zsh
+++ dot_config/zsh/lazy.zsh
@@ -132,7 +132,7 @@ export NODE_REPL_HISTORY="$XDG_STATE_HOME/node_history"
 export NPM_CONFIG_DIR="$XDG_CONFIG_HOME/npm"
 export NPM_DATA_DIR="$XDG_DATA_HOME/npm"
 export NPM_CACHE_DIR="$XDG_CACHE_HOME/npm"
-export NPM_CONFIG_USERCONFIG="$NPM_CONFIG_DIR/npmrc"
+export NPM_CONFIG_USERCONFIG="$NPM_CONFIG_DIR/.npmrc"
 
 ### PostgreSQL ###
 export PSQL_HISTORY="$XDG_STATE_HOME/psql_history"
